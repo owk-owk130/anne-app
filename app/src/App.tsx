e8247d3d75345cb68e4d1789c36f9ade9a4f193d
owk-owk130 <owaki-kengo@kayac.com>
2025-06-15 12:12:28 +0900

    tailwind化

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,15 +1,15 @@
-import { invoke } from '@tauri-apps/api/core';
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
+import { invoke } from "@tauri-apps/api/core";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState('');
-  const [name, setName] = useState('');
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke('greet', { name }));
+    setGreetMsg(await invoke("greet", { name }));
   }
 
   return (
